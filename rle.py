50a9ba67433c677330d655ec5e82c6f2cd9bc3b3
Izaak van Dongen <izaak.van.dongen@gmail.com>
2019-06-23 14:34:57 +0100

    2.4691358% optimisation

--- rle.py
+++ rle.py
@@ -1,11 +1,11 @@
-import sys as s
-import itertools as t
-x=s.stdin.buffer.read()
-o=s.stdout
-if '-d'in s.argv:
+from sys import*
+from itertools import*
+x=stdin.buffer.read()
+o=stdout
+if '-d'in argv:
  for n,c in zip(*[iter(x)]*2):
   o.write(chr(c)*n)
 else:
- for c,g in t.groupby(x):
+ for c,g in groupby(x):
   q,r=divmod(len([*g]),255)
   o.buffer.write(bytes([*[255,c]*q,r,c]))
