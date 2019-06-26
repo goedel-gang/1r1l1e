a3ed78478c2b242389c8ed101d0ac0b516cee0a1
Izaak van Dongen <izaak.van.dongen@gmail.com>
2019-06-26 09:31:39 +0100

    H U G E savings, Newlines are for nerds

--- rle.py
+++ rle.py
@@ -3,9 +3,6 @@ from itertools import*
 x=stdin.buffer.read()
 o=stdout
 if'-d'in argv:
- for n,c in zip(*[iter(x)]*2):
-  o.write(chr(c)*n)
+ for n,c in zip(*[iter(x)]*2):o.write(chr(c)*n)
 else:
- for c,g in groupby(x):
-  q,r=divmod(len([*g]),255)
-  o.buffer.write(bytes([*[255,c]*q,r,c]))
+ for c,g in groupby(x):q,r=divmod(len([*g]),255);o.buffer.write(bytes([*[255,c]*q,r,c]))
